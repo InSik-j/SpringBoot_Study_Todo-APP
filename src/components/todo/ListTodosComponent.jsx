c7473fa90738488cb3b02e8fb529c2ff3624c07e
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-04-02 14:35:51 +0900

    섹션12_Rest API로부터 데이터를 받아 Todo 출력

--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
+import { retrieveALLTodosForUsername } from "./api/TodoApiService";
+
 function ListTodosComponent(){
     const today = new Date();
     const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay());
-    const todos = [
-                    {id: 1, description: 'Learn AWS', done: false, targetDate: targetDate},
-                    {id: 2, description: 'Learn Full Strak Dev', done: false, targetDate: targetDate},
-                    {id: 3, description: 'Learn Full Strak DevOps', done: false, targetDate: targetDate},
-                
-                ]
 
+    const [todos, setTodos] = useState([])
+
+    useEffect( () => refreshTodos(), [])
+
+    function refreshTodos(){
+        retrieveALLTodosForUsername('in28minutes')
+            .then( (response) => {
+                console.log(response.data)
+                setTodos(response.data)
+            })
+            .catch( (error) => console.log(error) )
+            .finally( ()=> console.log('cleanup'));
+    }
+    
     return(
         <div className="container">
             <h1>Things you want To Do!</h1>
--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,20 +1,26 @@
 import {useParams, Link} from 'react-router-dom'
-import axios from 'axios'
+import { useState } from 'react';
+import { retrieveHelloWorldPathVariable } from './api/HelloWorldApiService';
 
 function WelcomeComponent(){
 
     const {username} = useParams();
+
+    const [message, setMessage] = useState(null)
     
     function callHelloWorldRestApi(){
         console.log('called')
-        axios.get('http://localhost:8080/hello-world')
-            .then( (response) => successfulResponse(response) )
-            .catch( (error) => errorResponse(error) )
-            .finally( ()=> console.log('cleanup'))
+        
+        retrieveHelloWorldPathVariable('Ranga')
+        .then( (response) => successfulResponse(response) )
+        .catch( (error) => errorResponse(error) )
+        .finally( ()=> console.log('cleanup'));
     }
 
     function successfulResponse(response){
         console.log(response)
+        //setMessage(response.data)
+        setMessage(response.data.message)
     }
     function errorResponse(error){
         console.log(error)
@@ -30,6 +36,7 @@ function WelcomeComponent(){
                     Call Hello World
                 </button>
             </div>
+            <div className='text-info'>{message}</div>
         </div>
         
     )
