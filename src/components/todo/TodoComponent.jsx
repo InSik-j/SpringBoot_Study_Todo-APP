345ce93d043781d3b6f05f87ce6202af610ad3b3
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-04-03 14:00:06 +0900

    섹션12_Todo 업데이트 기능 추가

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom"
-import { retrieveTodoApi } from './api/TodoApiService'
+import { useNavigate, useParams } from "react-router-dom"
+import { retrieveTodoApi, updateTodoApi } from './api/TodoApiService'
 import { useAuth } from "./security/AuthContext"
 import { useEffect, useState } from "react"
 import { Formik, Form, Field, ErrorMessage } from 'formik';
@@ -7,6 +7,8 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 export default function TodoComponent(){
 
     const {id} = useParams()
+
+    const navigate = useNavigate()
 
     const [description, setDescription] = useState('')
     const [targetDate, setTargetDate] = useState('')
@@ -30,6 +32,19 @@ export default function TodoComponent(){
 
     function onSubmit(values){
         console.log(values)
+        const todo ={
+            id : id,
+            username : username,
+            description: values.description,
+            targetDate: values.targetDate,
+            done: false
+
+        }
+        console.log(todo)
+        updateTodoApi(username, id, todo).then( response => {
+            navigate('/todos')
+        })
+        .catch( error => console.log(error) )
     }
     function validate(values){
         let errors ={
